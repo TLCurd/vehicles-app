0f7de8de6ed36c25bc0e49146a1c629fd71588d0
TLCurd <TLCurdChiro@gmail.com>
2022-04-20 21:42:04 -0600

    add working create request/action

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -3,4 +3,13 @@ class VehiclesController < ApplicationController
     @vehicles = Vehicle.all
     render template: "vehicles/index"
   end
+
+  def create
+    vehicle = Vehicle.new(make: params[:make], model: params[:model], vehicle_type: params[:vehicle_type], image_url: params[:image_url], years_made: params[:years_made])
+    if vehicle.save!
+      render json: {message: "The vehicle was successfully added!", vehicle: vehicle.as_json}
+    else
+      render json: {errors: vehicle.errors.full_messages}
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/vehicles", to: "vehicles#index"
+  post "/vehicles", to: "vehicles#create"
 end
